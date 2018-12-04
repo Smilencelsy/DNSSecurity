817a0affe6ee20963e7c79367dd51a07ed237af2
Smilencelsy <15273139330@163.com>
2018-12-04 21:34:55 +0800

    new codes

---
+++ code/model_training.py
@@ -0,0 +1,28 @@
+﻿from keras import models, layers
+from keras import optimizers
+import numpy as np
+
+def build_model(train_data):
+    model = models.Sequential()
+    model.add(layers.Dense(128, activation='relu', input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(3, activation='sigmoid'))   #标量回归
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    # model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='mse', metrics=['mae'])
+    return model
+
+#k折验证训练
+def k_train_data(train_data,train_targets,model):
+	k=4
+	num_epochs = 1
+	all_mae_histories = []
+	num_val_samples = len(train_data) // k  #k个分区，每个分区多少数据
+	for i in range(k):
+		print('processing fold #', i)
+		val_data = train_data[i * num_val_samples: (i+1) * num_val_samples]
+		val_targets = train_targets[i * num_val_samples: (i+1) * num_val_samples]
+		partial_train_data = np.concatenate([train_data[:i * num_val_samples], train_data[(i+1) * num_val_samples:]], axis = 0)
+		partial_train_targets = np.concatenate([train_targets[:i * num_val_samples], train_targets[(i+1) * num_val_samples:]], axis = 0)
+
+		model.fit(partial_train_data, partial_train_targets, validation_data=(val_data, val_targets), epochs=num_epochs, batch_size=1)
+	return model
---
+++ code/porn_gamble_detect.py
@@ -0,0 +1,102 @@
+﻿import pickle
+import numpy as np
+import linecache
+import model_training as mt
+
+def readFile():
+	with open('../source/word.pick','rb') as keyword_pick:
+		word_idx = pickle.load(keyword_pick)
+
+	#读取人工标记的训练数据word_x
+	word_dict={}
+	for index,word in enumerate(word_idx):
+		word_dict[word] = index
+		print word
+    
+	return word_dict,word_idx
+
+def gene_feature(word_dict,word_idx,count):
+	#将数据编码成特征向量
+	#特征向量采用一维one-hot编码
+	#其中0位表示字典中未出现的词
+	train_x=np.zeros((count,len(word_idx)+1))
+	train_y=np.zeros((count,3))
+	#共269个训练数据
+	for i in range(count):
+    	#奇数行分词
+    	#偶数行标签
+    	#1:色情 2:赌博 0:正常
+		x_line = 2*i+1
+		y_line = 2*i+2
+		label = linecache.getline('../source/word_x.txt',y_line)
+
+		label=label.strip('\n')
+
+		if label=='0':
+			train_y[i][0]=1
+		elif label=='1':
+			train_y[i][1]=1
+		elif label=='2':
+			train_y[i][2]=1
+
+		line = linecache.getline('../source/word_x.txt',x_line)
+		word_list = line.split(',')
+		for word in word_list:
+			if word_dict.get(word)!=None:
+				index = word_dict.get(word)
+				train_x[i][index] = 1
+			else :
+				train_x[i][0] = 1
+
+	#生成随机数据索引
+	#打乱训练数据
+	permutation = np.random.permutation(train_x.shape[0])
+	train_data = train_x[permutation, :]
+	train_targets = train_y[permutation]
+
+	return train_data,train_targets
+
+def predict_result(word_idx,word_dict,model):
+	#统计测试文件行数
+	with open("../source/word_5.txt") as f:
+		count = len(f.readlines())
+    
+	#处理测试数据
+	f = open('../source/label_5.txt','a')
+	for i in range(1,count):
+		if i%2000 == 0:
+			process = float(i)/float(count)
+			p = "%.2f%%" % (process * 100)
+			print p
+		if i%2 ==0 :
+			continue
+		line = linecache.getline('../source/word_5.txt',i)
+
+		word_list = line.split(',')
+		url = word_list[0]
+		test = np.zeros((1,len(word_idx)+1))
+		for word in word_list:
+			if word_dict.get(word)!=None:
+				index = word_dict.get(word)
+				test[0][index] = 1
+			else :
+				test[0][0] = 1
+
+		#计算模型结果
+		predict = model.predict(test)
+		label = np.argmax(predict)
+
+		f.write(url+","+str(label)+'\n')
+
+
+if __name__ == "__main__":
+	word_dict,word_idx = readFile()
+	with open("../source/word_x.txt") as f:
+		count = len(f.readlines())
+	train_data,train_targets = gene_feature(word_dict,word_idx,count/2)
+	model = mt.build_model(train_data)
+	model = mt.k_train_data(train_data,train_targets,model)
+	predict_result(word_idx,word_dict,model)
+      
+
+
